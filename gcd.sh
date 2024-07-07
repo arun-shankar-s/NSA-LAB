91506e21102801d5fe98246c9c7eb3b7af93c69b
Arun Shankar <145361756+arun-shankar-s@users.noreply.github.com>
2024-07-07 13:38:38 +0530

    Update gcd.sh

--- gcd.sh
+++ gcd.sh
@@ -1,1 +1,23 @@
+#!/bin/sh
 
+echo "Enter two numbers"
+read a b
+m=$a
+if [ $b -lt $m ];
+then
+m=$b
+fi
+
+while [ $m -ne 0 ];
+do
+x=$(( $a % $m ))
+y=$(( $b % $m ))
+
+if [ $x -eq 0 ] && [ $y -eq 0 ];
+then
+echo "gcd=$m"
+break
+fi
+
+m=$(($m-1))
+done
